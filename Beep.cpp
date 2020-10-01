3d22efd87312706158971a2474e20f21add19204
Haseeb Iqbal <72181793+haseebi@users.noreply.github.com>
2020-10-01 09:34:48 +0500

    Create Beep.cpp

---
+++ Beep.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <windows.h>   // WinApi header file
+using namespace std;
+int main(int argc, char *argv[])
+{
+  Beep(523.2511,500);  // 523.2511 hertz (C5) for 500 milliseconds
+  Beep(587.3295,500);
+  Beep(659.2551,500);
+  Beep(698.4565,500);
+  Beep(783.9909,500);
+  
+  cin.get(); // wait
+  return 0;
+}
